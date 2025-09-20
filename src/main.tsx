e81ead9b5878fa701c57911b1c11842e5c6ca6c3
HichemTab-tech <konanhichemsinshi@gmail.com>
2025-09-20 10:21:28 +0100

    Revert "Initial commit"
    This reverts commit 4b33816c750dc2dbe8760adb0c723f74271d4fbe.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import {createRoot} from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter, Route, Routes} from "react-router";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<App />} />
-      </Routes>
-  </BrowserRouter>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
